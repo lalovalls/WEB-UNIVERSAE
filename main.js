0bd992089c216c849ed8fc9de8988358a54cbaf7
AdriMartinez7 <Martinez2002@outlook.com>
2024-06-14 11:41:23 +0200

    6Push

--- main.js
+++ main.js
@@ -3,3 +3,40 @@ document.getElementById('logo-link').addEventListener('click', function(event) {
     const container = document.querySelector('.container');
     container.style.display = 'flex';
 });
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.popup-slides img');
+const popup = document.getElementById('popupCarrusel');
+const openPopup = document.getElementById('openPopup');
+const closePopup = document.querySelector('.close');
+
+// Mostrar pop-up
+openPopup.onclick = function() {
+    popup.style.display = 'block';
+    showSlides(slideIndex);
+}
+
+// Cerrar pop-up
+closePopup.onclick = function() {
+    popup.style.display = 'none';
+}
+
+// Cambiar slides
+function changeSlide(n) {
+    slideIndex += n;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    showSlides(slideIndex);
+}
+
+function showSlides(n) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[n].classList.add('active');
+}
+
+// Cerrar al hacer clic fuera del pop-up
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = 'none';
+    }
+}
