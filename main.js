80703ba1453c6d48c113cf1e361fd8ef4f2a7050
l4rry9912 <162343742+l4rry9912@users.noreply.github.com>
2024-06-14 13:44:12 +0200

    5Push

--- main.js
+++ main.js
@@ -4,27 +4,47 @@ document.getElementById('logo-link').addEventListener('click', function(event) {
     container.style.display = 'flex';
 });
 
-
 let slideIndex = 0;
 const slides = document.querySelectorAll('.popup-slides img');
 const popup = document.getElementById('popupCarrusel');
 const openPopup = document.getElementById('openPopup');
+const closePopup = document.querySelector('.close');
+
 openPopup.onclick = function() {
     popup.style.display = 'block';
     showSlides(slideIndex);
 }
+
+closePopup.onclick = function() {
+    popup.style.display = 'none';
+}
+function currentSlide(index) {
+    slideIndex = index;
+    showSlides(slideIndex);
+}
+
+function showSlides(n) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[n].classList.add('active');
+}
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = 'none';
+    }
+}
+
+document.querySelector('.prev').onclick = function() {
+    changeSlide(1);
+}
+
+document.querySelector('.next').onclick = function() {
+    changeSlide(-1);
+}
+
 function changeSlide(n) {
     slideIndex += n;
     if (slideIndex >= slides.length) slideIndex = 0;
     if (slideIndex < 0) slideIndex = slides.length - 1;
     showSlides(slideIndex);
-}
-function showSlides(n) {
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides[n].classList.add('active');
-}
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = 'none';
-    }
 }
